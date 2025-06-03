3c4637c61649a97c78cbc88ceab8292501c55359
Sillysally <92034603+SallyResch@users.noreply.github.com>
2025-06-03 13:10:14 +0200

    removed global body display flex to be able to add navbar in layout.js

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <Navbar />
       <h1>NutriCalc Pro</h1>
       <FoodSearchAddList />
     </div>
